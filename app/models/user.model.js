9e8ed50ffd0c061a8734fe6d25e859abc82693a0
Nhi <nhi19603@gmail.com>
2025-05-12 09:04:39 +0700

    update bcrypt

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const validator = require("validator");
 
 const salt = bcrypt.genSaltSync(10); // Tạo salt với độ khó 10
