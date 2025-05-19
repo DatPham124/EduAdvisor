c932c6a0d391a6a61f2ce4ac3af8e102c80a561f
Nhi <nhi19603@gmail.com>
2025-05-19 09:42:10 +0700

    update ui chatbot

--- src/views/Chatbot.jsx
+++ src/views/Chatbot.jsx
@@ -6,7 +6,21 @@ const Chatbot = () => {
         className="relative overflow-hidden bg-primary text-primary-color pt-[120px] md:pt-[130px] lg:pt-[160px]"
       >
         <div className="container relative z-10">
-          <div className="flex flex-col min-h-[75vh] justify-end bg-neutral-100 rounded-box mx-auto p-6 shadow-md mb-10 !z-10">
+          <div className="flex flex-col min-h-[75vh] justify-end bg-neutral-200 rounded-box mx-auto p-6 shadow-md mb-10 !z-10">
+            <div className="flex flex-col space-y-2 overflow-y-auto max-h-[60vh] mb-4 pr-2">
+              <div className="flex justify-start">
+                <div className="px-4 py-2 rounded-xl shadow max-w-[80%] break-words whitespace-pre-wrap bg-white text-black rounded-br-none">
+                  <p>chatbot message</p>
+                </div>
+              </div>
+
+              <div className="flex justify-end">
+                <div className="px-4 py-2 rounded-xl shadow max-w-[80%] break-words whitespace-pre-wrap bg-primary text-white rounded-br-none">
+                  <p>user message</p>
+                </div>
+              </div>
+            </div>
+
             <form action="#" method="POST" target="_blank" class="mt-8 flex">
               <input
                 type="text"
@@ -22,25 +36,35 @@ const Chatbot = () => {
                 <i class="lni lni-arrow-right"></i>
               </button>
               <div className="ml-4 flex justify-end">
-                <button 
-                className=" px-4 text-black text-lg/none text-center ">
-                  <svg width="35" height="35" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0 0 0)">
-                  <path d="M12 2.28125C9.37669 2.28125 7.25006 4.40788 7.25006 7.03122V11.5313C7.25006 14.1546 9.37669 16.2812 12 16.2812C14.6234 16.2812 16.75 14.1546 16.75 11.5313V7.03122C16.75 4.40788 14.6234 2.28125 12 2.28125Z" fill="#3657c3"/>
-                  <path d="M5.75 11.5312C5.75 11.117 5.41421 10.7812 5 10.7812C4.58579 10.7812 4.25 11.117 4.25 11.5312C4.25 15.5584 7.3217 18.8682 11.25 19.2454V20.7812H10C9.58579 20.7812 9.25 21.117 9.25 21.5312C9.25 21.9455 9.58579 22.2812 10 22.2812H14C14.4142 22.2812 14.75 21.9455 14.75 21.5312C14.75 21.117 14.4142 20.7812 14 20.7812H12.75V19.2454C16.6783 18.8682 19.75 15.5584 19.75 11.5312C19.75 11.117 19.4142 10.7812 19 10.7812C18.5858 10.7812 18.25 11.117 18.25 11.5312C18.25 14.983 15.4518 17.7812 12 17.7812C8.54822 17.7812 5.75 14.983 5.75 11.5312Z" fill="#3657c3"/>
+                <button className=" px-4 text-black text-lg/none text-center ">
+                  <svg
+                    width="35"
+                    height="35"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    transform="rotate(0 0 0)"
+                  >
+                    <path
+                      d="M12 2.28125C9.37669 2.28125 7.25006 4.40788 7.25006 7.03122V11.5313C7.25006 14.1546 9.37669 16.2812 12 16.2812C14.6234 16.2812 16.75 14.1546 16.75 11.5313V7.03122C16.75 4.40788 14.6234 2.28125 12 2.28125Z"
+                      fill="#3657c3"
+                    />
+                    <path
+                      d="M5.75 11.5312C5.75 11.117 5.41421 10.7812 5 10.7812C4.58579 10.7812 4.25 11.117 4.25 11.5312C4.25 15.5584 7.3217 18.8682 11.25 19.2454V20.7812H10C9.58579 20.7812 9.25 21.117 9.25 21.5312C9.25 21.9455 9.58579 22.2812 10 22.2812H14C14.4142 22.2812 14.75 21.9455 14.75 21.5312C14.75 21.117 14.4142 20.7812 14 20.7812H12.75V19.2454C16.6783 18.8682 19.75 15.5584 19.75 11.5312C19.75 11.117 19.4142 10.7812 19 10.7812C18.5858 10.7812 18.25 11.117 18.25 11.5312C18.25 14.983 15.4518 17.7812 12 17.7812C8.54822 17.7812 5.75 14.983 5.75 11.5312Z"
+                      fill="#3657c3"
+                    />
                   </svg>
-              </button>
+                </button>
               </div>
-              
             </form>
-             
           </div>
         </div>
         <div className="absolute bottom-5 left-96 z-0">
-            <img src="/images/dots.svg" alt="Dot" className=" w-25 opacity-75" />
-          </div>
-          <div className='absolute top-50 right-96 z-0'>
-              <img src='/images/dots.svg' alt="Dot" className="w-25 opacity-75" />
-          </div>
+          <img src="/images/dots.svg" alt="Dot" className=" w-25 opacity-75" />
+        </div>
+        <div className="absolute top-50 right-96 z-0">
+          <img src="/images/dots.svg" alt="Dot" className="w-25 opacity-75" />
+        </div>
       </div>
     </main>
   );
