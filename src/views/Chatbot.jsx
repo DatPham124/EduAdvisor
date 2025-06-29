99e4c52b5fb8931b8f054cd23110f865a7bcd54d
tub2105597 <tub2105597@student.ctu.edu.vn>
2025-06-29 15:58:17 +0700

    feat: Hien thi lich su tro chuyen khi load lai trang, hien thi voi cu phap Markdown

--- src/views/Chatbot.jsx
+++ src/views/Chatbot.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import ChatbotService from '../services/chatbot.service';
 import { franc } from 'franc'
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+
 
 const langMap = {
   eng: 'en-US',
@@ -23,13 +26,12 @@ const Chatbot = () => {
       try {
         const res = await ChatbotService.getConversation(registerAd._id);
         setMessages(res || []);
-        console.log("in response ", res[0].answer)
       } catch (error) {
         console.error('Lỗi lấy lịch sử tin nhắn:', error);
       }
     };
     fetchHistory();
-  }, [registerAd]);
+  }, []); // Chỉ chạy khi registerAd._id thay đổi
 
   const scrollToBottom = () => {
     const container = messagesEndRef.current; // truy cập DOM element được ref
@@ -65,7 +67,6 @@ const Chatbot = () => {
     });
 
     const formattedAnswer = res
-      .replace(/\*{1,2}\s*/g, ' ')
       .replace(/\n{1,2}/g, '\n')
       .trim();
 
@@ -191,7 +192,8 @@ const Chatbot = () => {
                 </div>
               </div>
 
-              {messages.map((msg, idx) => (
+              {
+                messages.map((msg, idx) => (
                 <div key={idx}>
                   <div className="flex justify-end">
                     <div className="px-4 py-2 my-2 rounded-xl shadow max-w-[80%] break-words whitespace-pre-wrap bg-primary text-white rounded-br-none">
@@ -200,7 +202,13 @@ const Chatbot = () => {
                   </div>
                   <div className="flex justify-start">
                     <div className="px-4 py-2 my-2 rounded-xl shadow max-w-[80%] break-words whitespace-pre-wrap bg-white text-black rounded-br-none">
-<p>{msg.answer === '...' ? <span className="animate-pulse">Đang phản hồi...</span> : msg.answer}</p>
+                      {msg.anwser === '...' ? <span className="animate-pulse">Đang phản hồi...</span> : 
+                      <div>
+                      <ReactMarkdown remarkPlugins={[remarkGfm]} >
+                            {msg.answer}
+                      </ReactMarkdown>
+                        </div>
+}
                     </div>
                     <div className="ms-4"></div>
                     <button
