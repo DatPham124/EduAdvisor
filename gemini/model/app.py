a8f2539dee17d5a84d0794fc70f6f31f8bc9e6b8
tub2105597 <tub2105597@student.ctu.edu.vn>
2025-07-01 19:45:00 +0700

    feat: Cai tien prompt ver 2.0, chinh sua du lieu

--- gemini/model/app.py
+++ gemini/model/app.py
@@ -8,6 +8,7 @@ from rapidfuzz import fuzz
 from unidecode import unidecode
 import time
 import re
+import tiktoken
 
 dotenv.load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -27,13 +28,19 @@ def normalize_text(text):
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
+def count_tokens_tiktoken(text):
+    encoding = tiktoken.get_encoding("cl100k_base")  # gần giống tokenizer của GPT-4/Gemini
+    tokens = encoding.encode(text)
+    return len(tokens)
+
 def find_intents(question, intent_list, user_info=None, faq_text=None, chat_session=None):
     start = time.time()
 
     prompt = f"""
         Bạn là chuyên gia phân tích ngữ cảnh cho chatbot tại Trung tâm CUSC.
         Bạn luôn xem lại toàn bộ đoạn hội thoại giữa người dùng và trợ lý AI để nhận diện ngữ cảnh của câu hỏi hiện tại.
-        Bạn sẽ nhận được một đoạn hội thoại và câu hỏi mới từ người dùng. Nhiệm vụ của bạn là xác định intent phù hợp nhất với câu hỏi mới dựa trên ngữ cảnh đã có.
+        Bạn sẽ nhận được một đoạn hội thoại và câu hỏi mới từ người dùng. 
+        Nhiệm vụ của bạn là xác định intent phù hợp nhất với câu hỏi mới dựa trên ngữ cảnh đã có.
 
         Bây giờ, người dùng vừa hỏi: "{question}"
 
@@ -92,26 +99,39 @@ def get_data_from_metadata(intents):
 
 def generate_answer(question, context, user_info=None, chat_session=None):
     start = time.time()
-    context = context[:2000] if context else ""
     prompt = f"""
     Luôn nhấn mạnh sau lời chào rằng bạn là một trợ lý AI hỗ trợ việc tư vấn tuyển sinh của Trung tâm công nghệ phần mềm CUSC.
-    Bạn chỉ tư vấn các thông tin về Trung tâm, các câu hỏi không liên quan đến trung tâm hãy trả lời là "Xin lỗi, tôi không thể giúp về vấn đề này.".
-
+    Bạn chỉ tư vấn các thông tin về Trung tâm, các câu hỏi không liên quan đến trung tâm hãy trả lời là "Xin lỗi, tôi không thể giúp về vấn đề này. Bạn hãy liên hệ với nhân viên tư vấn để được hỗ trợ.".
+    Nếu người dùng khen hay chê bạn, hãy trả lời là "Cảm ơn bạn đã phản hồi. Tôi sẽ cố gắng cải thiện hơn nữa.".
+    
     Thông tin dùng để trả lời:
     Thông tin người dùng: {user_info or "(Không có thông tin)"}
     Câu hỏi: "{question}"
     Ngữ cảnh: {context}. Nếu khôn có ngữ cảnh, hãy trả về kết quả là "Xin lỗi, tôi không thể trả lời câu hỏi này.".
 
     Nếu có ngữ cảnh thì trả lời theo hướng dẫn sau: 
-    - Bạn luôn trả lời ngắn gọn, chính xác và dễ hiểu.
+    - Bạn luôn trả lời ngắn gọn, chính xác, dễ hiểu và đúng trọng tâm với câu hỏi của người dùng.
     - Các từ về chuyên ngành, chức vụ vị trí, tên người phải được giữ nguyên.   
-    - Nếu người dùng hỏi về học phí, hãy hướng dẫn họ đến chatbot của tư vấn viên và vị trí là ngay dưới chatbot này.
+    - Khi người dùng hỏi về số lượng, hãy tính toán và trả lời chính xác.
+
+    Bên cạnh đó bạn phải tổng hợp thông tin từ các trang web chính thức của Trung tâm CUSC kết hợp với dữ liệu của ngữ cảnh để trả lời câu hỏi (nếu có). Dưới đây là danh sách các trang web chính thức mà bạn sẽ tham khảo:
+        - https://cusc.ctu.edu.vn/
+        - https://aptechcantho.cusc.vn/
+        - https://arenacantho.cusc.vn/
+        - http://www.cuscsoft.com/
+
+    - Nếu người dùng hỏi về học phí, hãy hướng dẫn họ liên hệ với với tư vấn viên qua các phương thức sau:
+        - Gọi điện thoại: 0292 383 5581 
+        - Liên hệ Hotline: 0901990665 - 0911204994
+        - Kết bạn qua Zalo: 0868 952 535 - 0868 952 545
   
    - Trình bày dạng danh sách nếu dữ liệu nhiều mục.
    - Trình bày dạng bảng khi so sánh.
 
     Nếu có thể, gợi ý câu hỏi tiếp theo ở cuối phần trả lời và cách biệt bằng dấu gạch ngang "---".
     """
+    count_token = count_tokens_tiktoken(prompt)
+    print(f"[Token] Tổng số token trong prompt generate_answer: {count_token}")
     try:
         response = chat_session.send_message(prompt)
         if response.candidates:
