d911d9d9ed1553336e2952acbffd65f9c2ad7b53
JenaT03 <nhi19603@gmail.com>
2025-07-06 18:26:36 +0700

    feat: change import

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Home/Header';
 import Footer from './components/Home/Footer';
 
-import Register from './views/Register';
 import Home from './views/Home';
 import Major from './views/Major';
 import MajorDetail from './views/MajorDetail'
-import Chatbot from './views/Chatbot'
 import DualMajorDetail from './views/DualMajorDetail'
 
 const App = () => {
@@ -19,10 +17,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/major" element={<Major />} />
-        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/major/:id" element={<MajorDetail />} />
         <Route path="/dualmajor/:id" element={<DualMajorDetail />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </div>
